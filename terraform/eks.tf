9936be3d658a4066d0fba41d49276b50f432b1a8
Ankita <ankitajaiswal0320@gmail.com>
2026-03-15 17:47:12 +0530

    initial eks devops project

---
+++ terraform/eks.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "devops-eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  enable_irsa = true
+
+  self_managed_node_groups = {
+    workers = {
+      name = "self-managed-workers"
+
+      instance_type = "t3.micro"
+
+      min_size     = 1
+      max_size     = 5
+      desired_size = 4
+
+      bootstrap_extra_args = ""
+
+      tags = {
+        Name = "self-managed-node"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
